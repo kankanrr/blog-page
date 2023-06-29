584eb34108471c9b3dfbf9b98cb8f44d55f55577
Sameer Mirza <srmirza14@gmail.com>
2023-06-28 22:16:28 -0400

    finished models folder | starting controllers

---
+++ models/Comment.js
@@ -0,0 +1,48 @@
+// vars
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection.js");
+
+// class creation
+
+class Comment extends Model {}
+
+// init
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    comment: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    postId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "post",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "comment",
+  }
+);
+
+module.exports = Comment;
